f73148fd4680956289c3cdb375e5813b0fa3301d
Bhavna Rathore <bhavnagithub@gmail.com>
2026-02-18 21:10:44 +0530

    Updated the api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+//Using axios.create() to make a reusable HTTP client.
+
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api", //baseURL points to your backend’s API endpoint.
   withCredentials: false, // or true if you use cookies
 });
 
